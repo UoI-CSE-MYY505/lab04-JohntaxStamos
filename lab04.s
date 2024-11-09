6ad0d430bfde8a5eafec0e4f57dd6042e7da3165
JohntaxStamos <cs05488@uoi.gr>
2024-11-09 17:53:54 +0200

    Add files via upload

--- lab04.s
+++ lab04.s
@@ -30,6 +30,17 @@ str_ge:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
+            lbu  t0, 0(a0)
+            lbu  t1, 0(a1)
+            sub  t2, t0, t1 # result 0
+            addi a0, a0, 1
+            addi a1, a1, 1
+            add  t3, t1, t0
+            beq  t3, t0, ret_strcmp # any string finished, leave
+            beq  t2, zero, str_ge # still equal, loop
+ret_strcmp:
+            srli a0, t2, 31 # get the sign bit
+            xori a0, a0, 1 # Invert it. So a0 is 1 if greater or equal
             jr   ra
  
 # ----------------------------------------------------------------------------
@@ -46,4 +57,26 @@ recCheck:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
-            jr   ra
+                slti t0, a1, 2 # check if size <= 2
+                beq  t0, zero, checkFirstTwo
+                addi a0, zero, 1 # return 1 
+                jr   ra
+checkFirstTwo:
+                addi sp, sp, -12 # make room for 3 in stack
+                sw   ra, 8(sp)
+                sw   a0, 4(sp)
+                sw   a1, 0(sp)
+                lw   a1, 0(a0) # 1st
+                lw   a0, 4(a0) # 2nd
+                jal  str_ge
+                beq  a0, zero, return # return 0, a0 is already 0
+                # do recursion
+                lw   a0, 4(sp) # get original a0 from stack
+                lw   a1, 0(sp) # get original a1 from stack
+                addi a0, a0, 4 # check the rest of the array, after 1st element
+                addi a1, a1, -1 # size-1
+                jal  recCheck
+return:
+                lw   ra, 8(sp)
+                addi sp, sp, 12 # adjust stack to delete 3 items
+                jr   ra
